06201fbd7c6bc2ae95acde635df77a5e9bda3579
Temur2006 <salnikovtemur2006@gmail.com>
2025-03-08 23:15:01 +0300

    s

---
+++ lw4-5/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+</head>
+<body>
+    <div>  
+        <img src="LogIm3.jpg"/>
+        <p>
+            <h1> 
+                <div>Эл. почтаff</div>
+                <input type="text">
+            </h1>
+            <h2>
+                <div>Введите эл. почту в формате ******@*******</div>
+            <h2>
+        </p>
+    </div>
+    <p>    
+        <h1>
+            <div>Пароль</div>
+            <input type="password">
+        </h1>
+        <h2>
+            <input type="submit"
+        </h2>    
+    </p>  
+</body>
+</html>
